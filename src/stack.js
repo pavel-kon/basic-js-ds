167dcaa20e2587ad8fb3ba02372dd7ac522c1fd1
Pavel <pav.konoval@gmail.com>
2023-04-16 14:32:52 +0300

    feat: Stack

--- src/stack.js
+++ src/stack.js
@@ -13,22 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	constructor() {
+		this.arr = [];
+	}
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	push(element) {
+		this.arr.push(element);
+	}
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	peek() {
+		return this.arr[this.arr.length - 1];
+	}
+
+	pop() {
+		return this.arr.pop();
+	}
 }
 
 module.exports = {
-  Stack
+	Stack,
 };
